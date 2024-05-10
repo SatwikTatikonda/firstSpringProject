04930356a088bdb00d4d26412f27e0a98124e867
SatwikTatikonda <satwk.tatikonda@gmail.com>
2024-05-10 19:42:36 +0530

    final chnages for may-10

--- src/main/java/com/scaler/demospringboot/helloService.java
+++ src/main/java/com/scaler/demospringboot/helloService.java
@@ -4,6 +4,8 @@ package com.scaler.demospringboot;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 public class helloService {
@@ -17,7 +19,12 @@ public class helloService {
     @RequestMapping(value="/helloOne",method= RequestMethod.GET)
     public String SayHelloOne(){
 
-        return "Hello World Satwik Tatikonda";
+        return "Hello World to all";
+    }
+    @RequestMapping(value="/helloOneId/{id}",method= RequestMethod.GET)
+    public String SayHelloOneId(@PathVariable("id") String id ){
+
+        return "Hello World "+id;
     }
 
 }
